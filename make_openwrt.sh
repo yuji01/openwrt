0ee58b45e8bff5b433d465773fe995bf9a2f26fa
yuji01 <59014140+yuji01@users.noreply.github.com>
2022-06-30 01:07:06 +0800

    Update make_openwrt.sh

--- make_openwrt.sh
+++ make_openwrt.sh
@@ -129,28 +129,28 @@ read -n 1 -p "请输入: "
             break
           ;;
           1)
-	    apt1 && echo -e "${GREEN}安装依赖--成功$END"||echo -e "${RED}安装依赖--失败$END"
+	        apt1 && echo -e "${GREEN}安装依赖--成功$END"||echo -e "${RED}安装依赖--失败$END"
           ;;
           2)
-	    git1 && echo -e "${GREEN}下载源码--成功$END"||echo -e "${RED}下载源码--失败$END"
+	        git1 && echo -e "${GREEN}下载源码--成功$END"||echo -e "${RED}下载源码--失败$END"
           ;;
           3)
-	    feed && echo -e "${GREEN}feed更新及安装--成功$END"||echo -e "${RED}feed更新及安装--失败$END"
+	        feed && echo -e "${GREEN}feed更新及安装--成功$END"||echo -e "${RED}feed更新及安装--失败$END"
           ;;
           4)
-	    config && echo -e "${GREEN}配置菜单--成功$END"||echo -e "${RED}配置菜单--失败$END"
+	        config && echo -e "${GREEN}配置菜单--成功$END"||echo -e "${RED}配置菜单--失败$END"
           ;;
           5)
-	    dl && echo -e "${GREEN}下载DL库--成功$END"||echo -e "${RED}下载DL库--失败$END"
+	        dl && echo -e "${GREEN}下载DL库--成功$END"||echo -e "${RED}下载DL库--失败$END"
           ;;
           6)
-	    make1 && echo -e "${GREEN}编译--成功$END" || echo -e "${RED}编译--失败$END"
+	        make1 && echo -e "${GREEN}编译--成功$END" || echo -e "${RED}编译--失败$END"
           ;;
           7)
-	    apt1 && git1 && feed && config && dl && make1
-		[ $? = 0 ] && echo -e "${GREEN}编译--成功$END" || echo -e "${RED}编译--失败$END"
+	        apt1 && git1 && feed && config && dl && make1
+		    [ $? = 0 ] && echo -e "${GREEN}编译--成功$END" || echo -e "${RED}编译--失败$END"
           ;;
-	  *)
+	      *)
             echo -e "$RED\t输入错误，请重新输入$END"
         esac
       done
@@ -167,7 +167,7 @@ read -n 1 -p "请输入: "
 	    (6) 配置菜单
 	    (7) 下载DL库
 	    (8) 编译
-	    (9) 全部执行
+		(9) 全部执行
 	    $END"
       read -n 1 -p "请输入: "
         case $REPLY in
@@ -175,34 +175,34 @@ read -n 1 -p "请输入: "
             break
           ;;
           1)
-	    dirclean2 && echo -e "${GREEN}清理所有编译文件--成功$END"||echo -e "${RED}清理所有编译文件--失败$END"
+	        dirclean2 && echo -e "${GREEN}清理所有编译文件--成功$END"||echo -e "${RED}清理所有编译文件--失败$END"
           ;;
           2)
-	    git2 && echo -e "${GREEN}git pull--成功$END"||echo -e "${RED}git pull--失败$END"
+	        git2 && echo -e "${GREEN}git pull--成功$END"||echo -e "${RED}git pull--失败$END"
           ;;
           3)
-	    src2 && echo -e "${GREEN}编译src-git--成功$END"||echo -e "${RED}编译src-git--失败$END"
+	        src2 && echo -e "${GREEN}编译src-git--成功$END"||echo -e "${RED}编译src-git--失败$END"
           ;;
           4)
-	    feed && echo -e "${GREEN}feed更新及安装--成功$END"||echo -e "${RED}feed更新及安装--失败$END"
+	        feed && echo -e "${GREEN}feed更新及安装--成功$END"||echo -e "${RED}feed更新及安装--失败$END"
           ;;
           5)
-	    defconfig2 && echo -e "${GREEN}make defconfig--成功$END"||echo -e "${RED}make defconfig--失败$END"
+	        defconfig2 && echo -e "${GREEN}make defconfig--成功$END"||echo -e "${RED}make defconfig--失败$END"
           ;;
           6)
-	    config && echo -e "${GREEN}配置菜单--成功$END"||echo -e "${RED}配置菜单--失败$END"
+	        config && echo -e "${GREEN}配置菜单--成功$END"||echo -e "${RED}配置菜单--失败$END"
           ;;
           7)
-	    dl && echo -e "${GREEN}下载DL库--成功$END"||echo -e "${RED}下载DL库--失败$END"
+	        dl && echo -e "${GREEN}下载DL库--成功$END"||echo -e "${RED}下载DL库--失败$END"
           ;;
           8)
-	    make2 && echo -e "${GREEN}编译--成功$END"||echo -e "${RED}编译--失败$END"
+	        make2 && echo -e "${GREEN}编译--成功$END"||echo -e "${RED}编译--失败$END"
           ;;
-	  9)
-	    dirclean2 && git2 && src2 && feed && defconfig2 && config && dl && make2
-	    [ $? = 0 ] && echo -e "${GREEN}编译--成功$END"||echo -e "${RED}编译--失败$END"
+		  9)
+	        dirclean2 && git2 && src2 && feed && defconfig2 && config && dl && make2
+		    [ $? = 0 ] && echo -e "${GREEN}编译--成功$END"||echo -e "${RED}编译--失败$END"
           ;;
-	  *)
+	     *)
             echo -e "$RED\t输入错误，请重新输入$END"
         esac
       done
@@ -229,7 +229,7 @@ read -n 1 -p "请输入: "
           3)
             re_config && echo -e "${GREEN}重新配置--成功$END"||echo -e "${RED}重新配置--失败$END"
           ;;
-	  *)
+	      *)
             echo -e "$RED\t输入错误，请重新输入$END"
         esac
       done
